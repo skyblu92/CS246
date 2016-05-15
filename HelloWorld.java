3c1662c14744cf96031e3f9a25856bd72452d1c1
skyblu92 <skyler.blumenthal@gmail.com>
2016-05-15 01:41:31 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello GitHub world!");
         // TODO code application logic here
     }
     
